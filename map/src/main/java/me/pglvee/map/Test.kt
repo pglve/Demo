46e02b295fb746c6de6b5840152e80bf63f9df0b
pglve <pglve@vip.qq.com>
2020-10-29 22:40:00 +0800

    增加map模块

---
+++ map/src/main/java/me/pglvee/map/Test.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020. pinggonglve
+ */
+
+package me.pglvee.map
+
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.mapboxsdk.plugins.china.shift.ChinaBoundsChecker
+
+
+
+//Determining whether coordinates are in China
+private fun t1(activity: AppCompatActivity, location: Location, lat: Double, lng: Double) {
+    // Pass in an Android-system Location object
+    val locationIsInChina = ChinaBoundsChecker.locationIsInChina(activity, location)
+
+    // Pass in a Mapbox LatLng object
+    //    val latLngIsInChina = ChinaBoundsChecker.latLngIsInChina(activity, LatLng(lat, lng))
+
+    // Pass in a Mapbox Point object
+    //    val pointIsInChina = ChinaBoundsChecker.pointIsInChina(activity, Point.fromLngLat(lng, lat))
+}
